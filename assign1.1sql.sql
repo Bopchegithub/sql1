d7e1ace93944f08e32a908ceeea7cb5782695651
Bopchegithub <96582690+Bopchegithub@users.noreply.github.com>
2022-02-11 21:03:22 +0530

    Add files via upload

---
+++ assign1.1sql.sql
@@ -0,0 +1,5 @@
+INSERT INTO `payaldb`.`employee11` (`idemployee`, `name`, `salary`, `dno`) VALUES ('101', 'Jones', '5500', '10');
+INSERT INTO `payaldb`.`employee11` (`idemployee`, `name`, `salary`, `dno`) VALUES ('102', 'Smith', '4500', '10');
+INSERT INTO `payaldb`.`employee11` (`idemployee`, `name`, `salary`, `dno`) VALUES ('103', 'Wills', '6500', '30');
+INSERT INTO `payaldb`.`employee11` (`idemployee`, `name`, `salary`, `dno`) VALUES ('104', 'John', '3500', '20');
+INSERT INTO `payaldb`.`employee11` (`idemployee`, `name`, `salary`, `dno`) VALUES ('105', 'Null', '4200', '30');
