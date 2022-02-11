12fa04710308303472c1c64a22b95e72945100d2
Bopchegithub <96582690+Bopchegithub@users.noreply.github.com>
2022-02-11 21:04:24 +0530

    Add files via upload

---
+++ assign1sql.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `payaldb`.`employee11` (
+  `idemployee` INT NOT NULL,
+  `name` VARCHAR(45) NULL,
+  `salary` VARCHAR(45) NULL,
+  `dno` VARCHAR(45) NULL,
+  PRIMARY KEY (`idemployee`));
